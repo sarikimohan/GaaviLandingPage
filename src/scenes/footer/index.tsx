507d3d48e3dffe4708dc364a2b7cbfd9e9e90820
sarikimohan <sariki.mohankrishna@gmail.com>
2024-01-09 19:15:49 +0530

    changes for deployment

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,8 +1,6 @@
-import Logo from "@/assets/Logo.png"
+import Logo from "@/assets/Logo.png";
 
-type Props = {}
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
     <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
@@ -10,8 +8,7 @@ const Footer = (props: Props) => {
           <img src={Logo} alt="logo" />
           <p className="my-5">
             faljlf;aglag;lalg lkjaklg ladsjlfalgj agalkgjlajf lakjg
-            alksdglajlfal akldjlfjal hdkfjklaj 
-            lkakg;ajdflka dfakhdflakjd f
+            alksdglajlfal akldjlfjal hdkfjklaj lkakg;ajdflka dfakhdflakjd f
           </p>
           <p>All Rights Reserves.</p>
         </div>
@@ -19,17 +16,17 @@ const Footer = (props: Props) => {
           <h4 className="font-bold">Links</h4>
           <p className="my-5">djla;f fad;</p>
           <p className="my-5">alf;kjl oiahfdj</p>
-          <p >fjlajflaf;</p>
+          <p>fjlajflaf;</p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-10">
           <h4 className="font-bold">Contact Us</h4>
           <p className="my-5">djla;f fad;</p>
           <p className="my-5">alf;kjl oiahfdj</p>
-          <p >76869585986</p>
+          <p>76869585986</p>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
